307ff9c50f1cc2ec3d28c420a5a0419dfcd64bcd
user <729193389@qq.com>
2020-07-22 16:31:17 +0800

    前台user

---
+++ app/Http/Controllers/Home/UserController.php
@@ -0,0 +1,14 @@
+<?php
+namespace app\Http\Controllers\Home;
+
+use app\Http\Controllers\Controller;
+
+class UserController extends Controller
+{
+
+    public function index()
+    {
+        echo 'user';
+    }
+
+}
